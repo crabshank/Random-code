115dbb918d2daf76ed8a7d1bfd8e933d31ff46ee
crabshank <44329154+crabshank@users.noreply.github.com>
2022-06-23 23:38:28 +0100

    Create  Set_environment_variables_per_exe.bat
    Sets "TMP" and "TEMP" environment for Chrome, as ""E:\Temp". To launch with administrator privileges, create a shortcut to this batch file and tick/check: Shortcut > Advanced > Run as administrator.

---
+++ Set_environment_variables_per_exe.bat
@@ -0,0 +1,5 @@
+@echo off
+set TEMP=E:\Temp
+setlocal
+set TMP=E:\Temp
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --no-default-browser-check --disable-web-security --allow-running-insecure-content
